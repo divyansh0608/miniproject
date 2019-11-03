2e59ffd8b4d488e093f3640e06df366cd8dd7ed4
divyansh0608 <divyansh8june@gmail.com>
2019-11-03 18:12:38 +0530

    added new changes 2

---
+++ DB/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const URI ="mongodb+srv://shopper:shoppass@shopdb-iuvtc.mongodb.net/test?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+  await mongoose.connect(URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  });
+  console.log('db connected..!');
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const connectDB = require('./DB/connection');
 const path = require('path')
-
 const app = express();
+connectDB();
 
-app.use(express.json())
+app.use(express.json({ extended: false }))
 app.use(express.urlencoded({extended: true}))
-
 app.use('/', express.static(path.join(__dirname, 'public')))
-app.use('/api', require('./routes/api').route)
 
-app.listen(2678, () => console.log('Server startled at http://localhost:2678')) 
+app.use('/api/userModel', require('./Api/User').route)
+app.use('/api/productModel', require('./Api/User').route)
+
+const Port = process.env.Port || 3000;
+app.listen(Port, () => console.log('Server started at http://localhost:2678')) 
+
