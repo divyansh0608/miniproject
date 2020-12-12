1d5a676e1195d39adef199a516260e7bb969cfb8
Divyansh singh panwar <31924580+divyansh0608@users.noreply.github.com>
2020-12-12 07:46:58 +0530

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const URI ="mongodb+srv://shopper:shoppass@shopdb-iuvtc.mongodb.net/test?retryWrites=true&w=majority";
-
 const connectDB = async () => {
   await mongoose.connect(URI, {
     useUnifiedTopology: true,
